a22b57daabdef68deb3d664e6c8c0dc07d914d67
Ingo Heimbach <i.heimbach@fz-juelich.de>
2018-02-19 17:32:21 +0100

    Increased version number

--- src/shallow-appify.py
+++ src/shallow-appify.py
@@ -24,7 +24,7 @@ logging.basicConfig(level=logging.WARNING)
 __author__ = 'Ingo Heimbach'
 __email__ = 'i.heimbach@fz-juelich.de'
 
-__version_info__ = (0, 1, 1)
+__version_info__ = (0, 2, 0)
 __version__ = '.'.join(map(str, __version_info__))
 
 
