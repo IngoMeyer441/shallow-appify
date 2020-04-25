c8565d85d987406353f8db12b446ff692593381a
Ingo Heimbach <i.heimbach@fz-juelich.de>
2020-04-25 12:03:45 +0200

    Overwork `setup.py`
    - Improve the extraction of the current version number
    - Read markdown from the README directly and do not use pandoc

--- setup.py
+++ setup.py
@@ -7,62 +7,51 @@ from __future__ import unicode_literals
 
 import codecs
 import os
-import subprocess
+import runpy
 from setuptools import setup, find_packages
-from shallow_appify._version import __version__
 
 
-def get_long_description_from_readme(readme_filename='README.md'):
-    rst_filename = '{}.rst'.format(os.path.splitext(os.path.basename(readme_filename))[0])
-    created_tmp_rst = False
-    if not os.path.isfile(rst_filename):
-        try:
-            subprocess.check_call(['pandoc', readme_filename, '-t', 'rst', '-o', rst_filename])
-            created_tmp_rst = True
-        except (OSError, subprocess.CalledProcessError):
-            pass
+def get_version_from_pyfile(version_file="shallow_appify/_version.py"):
+    file_globals = runpy.run_path(version_file)
+    return file_globals["__version__"]
+
+
+def get_long_description_from_readme(readme_filename="README.md"):
     long_description = None
-    if os.path.isfile(rst_filename):
-        with codecs.open(rst_filename, 'r', 'utf-8') as readme_file:
+    if os.path.isfile(readme_filename):
+        with codecs.open(readme_filename, "r", "utf-8") as readme_file:
             long_description = readme_file.read()
-    if created_tmp_rst:
-        os.remove(rst_filename)
     return long_description
 
 
+version = get_version_from_pyfile()
 long_description = get_long_description_from_readme()
 
 setup(
-    name='shallow-appify',
-    version=__version__,
+    name="shallow-appify",
+    version=version,
     packages=find_packages(),
     package_data={
-        str('shallow_appify'): ['dmg_background.png']  # setuptools needs byte strings as keys when running Python 2.x
+        str("shallow_appify"): ["dmg_background.png"]  # setuptools needs byte strings as keys when running Python 2.x
     },
-    install_requires=[
-        'Jinja2',
-        'Pillow'
-    ],
-    entry_points={
-        'console_scripts': [
-            'shallow-appify = shallow_appify.shallow_appify:main',
-        ]
-    },
-    author='Ingo Heimbach',
-    author_email='i.heimbach@fz-juelich.de',
-    description='Converts any executable to a non-self-contained mac app bundle which depends on system libraries.',
+    install_requires=["Jinja2", "Pillow"],
+    entry_points={"console_scripts": ["shallow-appify = shallow_appify.shallow_appify:main",]},
+    author="Ingo Heimbach",
+    author_email="i.heimbach@fz-juelich.de",
+    description="Converts any executable to a non-self-contained mac app bundle which depends on system libraries.",
     long_description=long_description,
-    license='MIT',
-    url='https://github.com/IngoHeimbach/shallow-appify',
-    keywords=['macOS', 'app', 'py2app'],
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/IngoHeimbach/shallow-appify",
+    keywords=["macOS", "app", "py2app"],
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: MacOS',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Topic :: Utilities'
-    ]
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Utilities",
+    ],
 )
